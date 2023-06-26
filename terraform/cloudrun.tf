dc07d17d59e5574ee65bcb15b41eba4f36934711
Matt Schiros <schiros@gmail.com>
2023-06-25 18:49:28 -0700

    oauth iap binding

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -11,7 +11,7 @@ resource "google_cloud_run_service" "chatgproxyt_api" {
 
 	metadata {
 		annotations = {
-			"run.googleapis.com/ingress" = "all"
+			"run.googleapis.com/ingress" = "internal-and-cloud-load-balancing"
 		}
 	}
 
--- terraform/lb.tf
+++ terraform/lb.tf
@@ -3,6 +3,7 @@ resource "google_compute_backend_bucket" "frontend_bucket" {
 	description = "Frontend for chatgproxyt"
 	bucket_name = google_storage_bucket.hosting_bucket.name
 	enable_cdn = true
+
 }
 
 resource "google_compute_url_map" "frontend_map" {
@@ -25,7 +26,6 @@ resource "google_compute_target_https_proxy" "frontend" {
 	name = "chatgproxyt-frontend"
 	url_map = google_compute_url_map.frontend_map.id
 	certificate_map = format("//%s/%s", "certificatemanager.googleapis.com" , google_certificate_manager_certificate_map.chatgproxyt.id)
-
 }
 
 resource "google_compute_backend_service" "chatgproxyt_api" {
@@ -33,6 +33,12 @@ resource "google_compute_backend_service" "chatgproxyt_api" {
 	protocol = "HTTP"
 	port_name = "http"
 	timeout_sec = 30
+	
+	iap {
+		oauth2_client_id = local.oauth2_client_id
+		oauth2_client_secret = local.oauth2_client_secret
+	}
+
 	backend {
 		group = google_compute_region_network_endpoint_group.chatgproxyt_cloudrun_neg.id
 	}
